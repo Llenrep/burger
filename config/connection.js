435b95f0cb9844ec9d8bdbcd6cc2f465eb5e2627
Llenrep <lppernell1998@gmai.com>
2018-12-22 00:14:21 -0500

    added if statement for jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,21 @@
 var mysql = require('mysql');
 
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-var connection = mysql.createConnection({
-    host: "localhost",
+    var connection = mysql.createConnection({
+        host: "localhost",
 
-    port: 8080,
+        port: 8080,
 
-    user: "root",
+        user: "root",
 
-    password: "root",
-    
-    database: "burger_db"
-});
+        password: "root",
+
+        database: "burger_db"
+    });
+}
 
 connection.connect(function (err) { //this portion simply establishes the connection to MAMP
     if (err) throw err;
